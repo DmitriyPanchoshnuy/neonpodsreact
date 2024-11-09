7edbb9803d3ddc957b894027791c38c7e8e03930
DmitriyPanchoshnuy <dmitriypanchoshnuy@outlook.com>
2024-11-10 01:19:25 +0200

    Update category.jsx

--- src/components/Category/category.jsx
+++ src/components/Category/category.jsx
@@ -1,9 +1,9 @@
 import { Component } from "react";
 import "./category.css";
 import "bootstrap-icons/font/bootstrap-icons.css"
-import { useNavigate } from "react-router-dom";
 
 export class Category extends Component {
+
     render() {
         return (
             <div className="category">
