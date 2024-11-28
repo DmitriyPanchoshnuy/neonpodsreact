cf22e04c6484ba54156565d18fd25ba76cf31e39
DmitriyPanchoshnuy <dmitriypanchoshnuy@outlook.com>
2024-11-28 22:35:16 +0200

    modigied

--- src/Pages/GeneralPage/GeneralPage.jsx
+++ src/Pages/GeneralPage/GeneralPage.jsx
@@ -1,9 +1,27 @@
 import "./GeneralPage.css";
+import { useEffect, useState } from "react";
+
+import { getSections } from "../../database/api.js"
 
 export default function GeneralPage(props) {
-    return (
-        <div className="GeneralPageContainer">
-            Що ви бажаєте замовити?
-        </div>
-    )
+  const [sections, setSections] = useState([])
+
+  useEffect(() => {
+    getSections().then((data) => {
+      setSections(data)
+    })
+  }, [])
+
+  return (
+    <div className="GeneralPageContainer">
+      <p className="">Що ви бажаєте замовити?</p>
+      <p>Можливо замовити доставку або самовиніс.</p>
+      <p>Оберіть бажану вами категорію</p>
+      <div className="">
+        {sections.map((section) => (
+          <span>{section.title}</span>
+        ))}
+      </div>
+    </div>
+  )
 }
