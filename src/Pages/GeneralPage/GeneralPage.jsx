a4d6c883c5342ea9cdb56899b993beffa5ca4d1b
DmitriyPanchoshnuy <dmitriypanchoshnuy@outlook.com>
2024-11-28 23:25:52 +0200

    s

--- src/Pages/GeneralPage/GeneralPage.jsx
+++ src/Pages/GeneralPage/GeneralPage.jsx
@@ -1,14 +1,17 @@
 import "./GeneralPage.css";
 import { useEffect, useState } from "react";
 
+import { Link, useNavigate } from "react-router-dom"
 import { getSections } from "../../database/api.js"
 
 export default function GeneralPage(props) {
-  const [sections, setSections] = useState([])
+  const [sections, setSections] = useState([]);
+
+  const navigator = useNavigate();
 
   useEffect(() => {
     getSections().then((data) => {
-      setSections(data)
+      setSections(data);
     })
   }, [])
 
@@ -17,9 +20,11 @@ export default function GeneralPage(props) {
       <p className="">Що ви бажаєте замовити?</p>
       <p>Можливо замовити доставку або самовиніс.</p>
       <p>Оберіть бажану вами категорію</p>
-      <div className="">
+      <div className="SectionList">
         {sections.map((section) => (
-          <span>{section.title}</span>
+          <div className="SectionItem">
+            {section.title}
+          </div>
         ))}
       </div>
     </div>
